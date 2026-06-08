430a20115ddba2620c74ed26deb527ba5c2784bd
Ismail <yotech42@gmail.com>
2026-06-08 19:38:10 +0600

    Add same-repo Homebrew formula

---
+++ Formula/end-port.rb
@@ -0,0 +1,24 @@
+class EndPort < Formula
+  desc "Tiny native tray utility for ending local development ports"
+  homepage "https://github.com/6space7/end-port"
+  url "https://github.com/6space7/end-port/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "9241a544a2fc9e5b32239a32e50fe61cd068c6d43c7684d755d68914b95197c7"
+  license any_of: ["MIT", "Apache-2.0"]
+  head "https://github.com/6space7/end-port.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  on_linux do
+    depends_on "pkgconf" => :build
+    depends_on "gtk+3"
+    depends_on "libayatana-appindicator"
+  end
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    assert_match "Start the tray/menu-bar utility", shell_output("#{bin}/end-port --help")
+  end
+end
