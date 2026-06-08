8dc4605df8ea46241b8d670f0688fb021bd378de
Ismail <yotech42@gmail.com>
2026-06-08 20:18:40 +0600

    Clean up Homebrew cask audit

--- Casks/end-port.rb
+++ Casks/end-port.rb
@@ -2,8 +2,7 @@ cask "end-port" do
   version "0.3.0"
   sha256 "4674f597b9997a35b2ab8554e1f45401800905122edbc0eb7eb9f434d8ea3b27"
 
-  url "https://github.com/6space7/end-port/releases/download/v#{version}/End-Port-#{version}-macos-arm64.zip",
-      verified: "github.com/6space7/end-port/"
+  url "https://github.com/6space7/end-port/releases/download/v#{version}/End-Port-#{version}-macos-arm64.zip"
   name "End Port"
   desc "Menu bar utility for ending local development ports"
   homepage "https://github.com/6space7/end-port"
